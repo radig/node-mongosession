22ed2d002d1968e6aea9825314f9317215b031a7
Cauan Cabral <cauan@radig.com.br>
2011-12-22 12:25:08 -0300

    Refatoração da lib
    Melhor documentação no código
    Trabalhando de forma assincrona corretamente
    Organização e limpeza de código

--- mongo_sessions.js
+++ mongo_sessions.js
@@ -12,6 +12,10 @@
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 
+var Db = require('mongodb').Db,
+	Server = require('mongodb').Server,
+	util = require('util');
+
 /**
  * Classe para validação de sessão no banco de dados
  * 
@@ -21,33 +25,95 @@
  * 
  */
 var MongoSession = exports.MongoSession = function(config) {
-	/**
-	 * Carregamento das classes necessárias a comunicação com MongoDB
-	 * Utiliza o módulo mongodb-native para conexão (instalar via NPM).
-	 */ 
-	var Db = require('mongodb').Db,
-		Server = require('mongodb').Server;
 	
 	this.settings = {
-		db: {
-			host: 'localhost',
-			port: '27017',
-			database: 'default',
-			collection: 'default',
-			username: false,
-			password: false
-		},
-		cipher: {
-			iv: '',
-			key: ''
+		host: 'localhost',
+		port: 27017,
+		database: 'default',
+		collection: 'default',
+		username: false,
+		password: false
+	};
+	
+	/**
+	 * 
+	 * @var Db interface com MongoDB
+	 */
+	this.MongoDB = null;
+	
+	/**
+	 *
+	 * @var Collection interface direta com uma coleção
+	 * do MongoDB
+	 */
+	this.data = null;
+	
+	/**
+	 * Efetua conexão com banco de dados que contém
+	 * dados da sessão.
+	 * 
+	 * Conexão fica aberta, disponível para verificação
+	 * da sessão.
+	 */
+	this.connect = function() {
+		var self = this;
+
+		self.MongoDB.open(function(err, db) {
+			if(db === null) {
+				util.log("Session: Não foi possível abrir o BD.");
+				util.log(err);
+			}
+			else {
+				db.collection(self.settings.collection, function(err, collection) {
+					if(collection === null) {
+						util.log("Session: Não é possível acessar a coleção.");
+						util.log(err);
+					}
+					else {
+						self.data = collection;
+					}
+				});
+			}
+		});
+	};
+	
+	/**
+	 * Método responsável por retornar um determinado valor da sessão
+	 * salva no banco.
+	 *  
+	 * Recebe como parâmetro o id da sessão (session) e o campo (field) que deverá
+	 * ser passado ao callback (callback).
+	 * 
+	 * @param obj Object Classe que contém o callback
+	 * @param session String
+	 * @param field String
+	 * @param callback Function
+	 * @param pass Object
+	 */
+	this.check = function(obj, session, field, callback, pass) {
+		var self = this;
+		
+		if(self.data !== null) {
+			
+			self.data.findOne({'_id': session}, function(err, doc) {
+				if(doc != null && typeof doc != 'undefined' && typeof doc[field] != 'undefined') {
+					self.value = doc[field];
+
+					callback(obj, doc[field], pass);
+				}
+				else {
+					util.log(err);
+				}
+			});
 		}
 	};
 	
-	this.db = null;
-
-	this.value = null;
-	
-	this.conn = null;
+	// --------------------------------------------------------------------
+	// 
+	// Daqui em diante pode ser considerado o construtor do objeto.
+	// Inclua tudo que deve ser executado ao instanciar a classe.
+	//
+	// --------------------------------------------------------------------
 	
 	/**
 	 * Mescla recursivamente os atributos de dois objetos
@@ -71,80 +137,13 @@ var MongoSession = exports.MongoSession = function(config) {
 	};
 	
 	/**
-	 * Efetua conexão com banco de dados que contém
-	 * dados da sessão.
-	 * 
-	 * Conexão fica aberta, disponível para verificação
-	 * da sessão.
-	 */
-	this.connect = function() {
-		var self = this;
-		
-		if(self.db === null) {
-			self.conn.open(function(err, db) {
-				if(db === null)
-				{
-					console.log("Session: Não foi possível abrir o BD.");
-					console.log(err);
-					
-					return false;
-				}
-				
-				self.db = db;
-			});
-		}
-	};
-	
-	/**
-	 * Método responsável por retornar um determinado valor da sessão
-	 * salva no banco.
-	 *  
-	 * Recebe como parâmetro o id da sessão (session) e o campo (field) que deverá
-	 * será passado ao callback (callback).
-	 */
-	this.check = function(session, field, callback) {
-		var self = this;
-		
-		if(self.db === null) {
-			console.log("Session: É preciso conectar ao Banco antes de utilizar seus dados.");
-			return false;
-		}
-		
-		self.db.collection(this.settings.db.collection, function(err, collection) {
-			if(collection === null)
-			{
-				console.log("Session: Não é possível acessar a coleção.");
-				console.log(err);
-					
-				return false;
-			}
-			
-			collection.findOne({'_id': session}, function(err, doc) {
-				
-				if(typeof doc != 'undefined' && typeof doc[field] != 'undefined') {
-					self.value = doc[field];
-					
-					callback(err, doc[field]);
-				}
-				else {
-					callback(err, null);
-				}
-				
-			});
-		});
-		
-		return self.value;
-	};
-	
-	/**
 	 * Inicialização dos atributos
 	 */
-	
 	if(typeof config != 'undefined' && config !== null) {
 		this.settings = this.mergeProperties(this.settings, config);
 	}
 	
-	this.conn = new Db(this.settings.db.database, new Server(this.settings.db.host, this.settings.db.port, {}));
+	this.MongoDB = new Db(this.settings.database, new Server(this.settings.host, this.settings.port, {}));
 	
 	this.connect();
 };
